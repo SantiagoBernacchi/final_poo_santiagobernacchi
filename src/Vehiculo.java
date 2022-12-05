41e6d88d0730b4f91a21db8fc6ca365db9a870de
Santiago Bernacchi <santiago.bernacchi@davinci.edu.ar>
2022-12-04 21:51:12 -0300

    feat: creamos clase de vehiculo

---
+++ src/Vehiculo.java
@@ -0,0 +1,38 @@
+public class Vehiculo {
+    private int cantRueda;
+    private double kilometros;
+    private String marca;
+    private String modelo;
+
+    public int getCantRueda() {
+        return cantRueda;
+    }
+
+    public void setCantRueda(int cantRueda) {
+        this.cantRueda = cantRueda;
+    }
+
+    public double getKilometros() {
+        return kilometros;
+    }
+
+    public void setKilometros(double kilometros) {
+        this.kilometros = kilometros;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+}
