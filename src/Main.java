adee713ecb5375a66c46ef2a91a424d3156c7c3d
Santiago Bernacchi <santiago.bernacchi@davinci.edu.ar>
2022-12-04 21:51:54 -0300

    feat: creamos clase main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[]args) {
+        Garaje garaje1= new Garaje(20, 1500);
+        Auto auto1= new Auto("Nissan", "Sentra",25000,4,5);
+        Moto moto1= new Moto("Honda","Tornado",2,250);
+
+    garaje1.agregarVehiculo(auto1);
+    garaje1.agregarVehiculo(auto1);
+    garaje1.agregarVehiculo(auto1);
+
+    System.out.println(String.format(""));
     }
 }
