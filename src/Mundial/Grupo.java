863c67b885e574296e1db3ba6dea351e863aeb7c
Santiago Bernacchi <santiago.bernacchi@davinci.edu.ar>
2022-12-06 20:00:16 -0300

    feat: llenamos clase Grupo

--- src/Mundial/Grupo.java
+++ src/Mundial/Grupo.java
@@ -1,4 +1,11 @@
 package Mundial;
 
-public class Grupo {
-}
+import java.util.ArrayList;
+
+public class Grupo extends Etapa{
+        @Override
+        public ArrayList<Equipo> getEquiposQueAvanzan() {
+            return super.getEquiposQueAvanzan();
+        }
+    }
+
