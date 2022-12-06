4c994f418968bd1b5f0d4c375a332deaa983a8f9
Santiago Bernacchi <santiago.bernacchi@davinci.edu.ar>
2022-12-06 19:59:39 -0300

    feat: llenamos clase equipo

--- src/Mundial/Equipo.java
+++ src/Mundial/Equipo.java
@@ -4,5 +4,18 @@ import java.util.ArrayList;
 
 public class Equipo {
 
-    
+    private String nombre;
+    private ArrayList<Partido> partidosJugados;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Equipo(String nombre) {
+        this.setNombre(nombre);
+    }
 }
