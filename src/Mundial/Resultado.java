3fb3e72f8d1fbbac12213e46253fafeeddfa0200
Santiago Bernacchi <santiago.bernacchi@davinci.edu.ar>
2022-12-06 20:00:37 -0300

    feat: llenamos clase Resultado

--- src/Mundial/Resultado.java
+++ src/Mundial/Resultado.java
@@ -1,4 +1,30 @@
 package Mundial;
 
 public class Resultado {
+    private int golesLocal;
+    private int golesVisitante;
+
+
+    //Setters y Getters de golesLocal-golesVisitante
+    public int getGolesLocal() {
+        return golesLocal;
+    }
+
+    public void setGolesLocal(int golesLocal) {
+        this.golesLocal = golesLocal;
+    }
+
+    public int getGolesVisitante() {
+        return golesVisitante;
+    }
+
+    public void setGolesVisitante(int golesVisitante) {
+        this.golesVisitante = golesVisitante;
+    }
+
+    //Boolean de quien gano
+    public boolean ganoLocal() {
+        return golesLocal>golesVisitante;
+
+    }
 }
