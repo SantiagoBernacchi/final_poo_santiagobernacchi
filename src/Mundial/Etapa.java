87dba53d6ebf7c432476efc59717024dcc5248f9
Santiago Bernacchi <santiago.bernacchi@davinci.edu.ar>
2022-12-06 20:00:09 -0300

    feat: llenamos clase Etapa

--- src/Mundial/Etapa.java
+++ src/Mundial/Etapa.java
@@ -1,4 +1,44 @@
 package Mundial;
 
+import java.util.ArrayList;
+
 public class Etapa {
+
+    private String infoEtapa;
+    private ArrayList<Partido> listaDepartidos;
+
+    private ArrayList<Equipo> equiposQueAvanzan;
+
+    //Metodo Constructor EtapaMundial
+
+    Etapa(){
+        this.listaDepartidos = new ArrayList<Partido>();
+        this.equiposQueAvanzan = new ArrayList<Equipo>();
+    }
+
+
+
+    public String getInfoEtapa() {
+        return infoEtapa;
+    }
+
+    public void setInfoEtapa(String infoEtapa) {
+        this.infoEtapa = infoEtapa;
+    }
+
+
+
+    public ArrayList<Partido> getListaDepartidos() {
+        return listaDepartidos;
+    }
+
+
+    public ArrayList<Equipo> getEquiposQueAvanzan() {
+        return equiposQueAvanzan;
+    }
+
+    public void setEquiposQueAvanzan(ArrayList<Equipo> equiposQueAvanzan) {
+        this.equiposQueAvanzan = equiposQueAvanzan;
+    }
+
 }
